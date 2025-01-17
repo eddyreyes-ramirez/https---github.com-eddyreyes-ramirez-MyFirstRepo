e38b3a1397fb8e13187f23a4eccb1c1ab6247ea5
eddyreyes-ramirez <reyes.eddy@ymail.com>
2025-01-17 09:31:07 -0500

    New folder named First Repo created

---
+++ Module 1/Module 1/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Does this work?");
+        System.out.println("Last Check prior to submission.");
+    }
+}
---
+++ MyFirstRepo/Init/src/MyFirstRepo.java
@@ -0,0 +1,7 @@
+public class MyFirstRepo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("My first Reposistory is a pain," +
+        "Lets see if this uploads");
+    }
+}
