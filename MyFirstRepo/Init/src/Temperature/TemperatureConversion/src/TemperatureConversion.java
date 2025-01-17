9be4a4f9a70c6e61a7c3eae48066a8ad8acef0ca
eddyreyes-ramirez <reyes.eddy@ymail.com>
2025-01-17 16:57:28 -0500

    Comments added

---
+++ MyFirstRepo/Init/src/Temperature/TemperatureConversion/src/TemperatureConversion.java
@@ -0,0 +1,32 @@
+//program will convert Celsius to Fahrenheit
+//importing all classes from java.util
+import java.util.*;
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        // create scanner to allow input by user
+        Scanner input = new Scanner(System.in);
+        //create variable and assigned value of y
+        String optionToContinue = "Y";
+        
+        // creating sentinel value to give program a way to exit
+        // while user presses y, or Y, will continue to use program
+
+        while (optionToContinue.equalsIgnoreCase("Y")) {
+            // asks user to enter temperature in Celsius
+         System.out.println("Enter the temperature in Celsius." +
+         " Ex.24.2.");
+        // variable of type double, will be assigned to the user's next input   
+        double celsiusInput = input.nextDouble();
+        //variable of type double, formula for conversion using the user's celsius input
+        double TempInFahrenheit = (9.0/5) * celsiusInput + 32;
+        
+         
+         
+         System.out.print(celsiusInput + " degrees celsius is " + (String.format("%.2f",TempInFahrenheit)) + " degrees fahrenheit.\n" +
+         "Do you want to play again? Press (Y)es pr (N)o to exit\n");
+         input.nextLine();
+         optionToContinue = input.nextLine();
+        }
+    input.close();            
+    }
+}
