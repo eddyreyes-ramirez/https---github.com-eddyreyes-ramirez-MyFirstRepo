0e09a56e08acd7ad582f695d94a02d078cf451df
eddyreyes-ramirez <reyes.eddy@ymail.com>
2025-01-17 14:28:08 -0500

    Repetition File created

---
+++ MyFirstRepo/Init/src/WelcomeProject/src/Repetition.java
@@ -0,0 +1,11 @@
+public class Repetition { // class name is Repetition
+    public static void main(String[] args) { 
+        // count of type integer assigned zero
+        int count = 0;
+        // while condition is met, count less than 5, print 5 times.
+        while (count < 5) { 
+            System.out.println("Welcome to Java.");
+            count++; //add one to the count and continue loop
+        }
+    }
+}
