0ba8b29e3d7291ed50cdeadb43efb0d456b225a9
eddyreyes-ramirez <reyes.eddy@ymail.com>
2025-01-22 09:25:02 -0500

    final comments added, good to submit

--- MyFirstRepo/Init/src/Temperature/TemperatureConversion/src/TemperatureConversion.java
+++ MyFirstRepo/Init/src/Temperature/TemperatureConversion/src/TemperatureConversion.java
@@ -5,28 +5,27 @@ public class TemperatureConversion {
     public static void main(String[] args) {
         // create scanner to allow input by user
         Scanner input = new Scanner(System.in);
-        //create variable and assigned value of y
+        // creating sentinel value to give program a way to exit
         String optionToContinue = "Y";
         
-        // creating sentinel value to give program a way to exit
         // while user presses y, or Y, will continue to use program
 
         while (optionToContinue.equalsIgnoreCase("Y")) {
             // asks user to enter temperature in Celsius
-         System.out.println("Enter the temperature in Celsius." +
-         " Ex.24.2.");
-        // variable of type double, will be assigned to the user's next input   
-        double celsiusInput = input.nextDouble();
-        //variable of type double, formula for conversion using the user's celsius input
-        double TempInFahrenheit = (9.0/5) * celsiusInput + 32;
-        
-         
-         
-         System.out.print(celsiusInput + " degrees celsius is " + (String.format("%.2f",TempInFahrenheit)) + " degrees fahrenheit.\n" +
-         "\nDo you want to play again? Press (Y)es or (N)o to exit\n");
-         input.nextLine();
-         optionToContinue = input.nextLine();
+            System.out.println("Enter the temperature in Celsius." +
+             " Ex.24.2.");
+            // variable of type double, will be assigned to the user's next input   
+            double celsiusInput = input.nextDouble();
+            //variable of type double, formula for conversion using the user's celsius input
+            double TempInFahrenheit = (9.0/5) * celsiusInput + 32;
+            //print final conversion statement and asks user if they wish to continue using sentinel value as a way to end
+            System.out.print(celsiusInput + " degrees celsius is " + (String.format("%.2f",TempInFahrenheit))
+             + " degrees fahrenheit.\n" +
+             "\nDo you want to play again? Press (Y)es or (N)o to exit\n");
+            input.nextLine();
+            optionToContinue = input.nextLine();
         }
-    input.close();            
+        //close scanner
+    input.close();          
     }
 }
