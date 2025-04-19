7f062452d5a08f677a2a98c00cd4d6e3a7c7a5c2
eddyreyes-ramirez <reyes.eddy@ymail.com>
2025-04-19 14:20:36 -0400

    Comments Updated

--- MyFirstRepo/Init/src/eliminatingDuplicates/eliminatingDuplicates/src/eliminatingDuplicates.java
+++ MyFirstRepo/Init/src/eliminatingDuplicates/eliminatingDuplicates/src/eliminatingDuplicates.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class eliminatingDuplicates {
     public static void main(String[] args) throws Exception {
-
+        //open Scanner for user input
         Scanner in = new Scanner(System.in);
+        //create array, of type int, will hold 10 values
         int [] values = new int [10];
-
+        //ask user to enter 10 values 
          System.out.print("Enter ten values. Press enter after every value.\n");
          //for loop, variable i = 0, adds user input directly into array, values,
          for (int i =0; i < values.length; i++) { 
@@ -16,7 +17,7 @@ public class eliminatingDuplicates {
          //print distinct numbers
         System.out.print("The distinct numbers are: ");
         for (int number: result){
-                System.out.println(number + "");
+                System.out.print(number + " ");
         }
         //close scanner
         in.close();
@@ -39,15 +40,17 @@ public class eliminatingDuplicates {
            }
            //copys only distinct values into array named result
            int [] result = new int [count];
+           //loop through array from 0 to size -1
            for(int i=0; i<count; i++){
             result [i] = distinctList[i];
            }
+           //return array 
            return result;
         }
-        //
+        // method linear search
         public static int linearSearch(int[]array, int size, int key){
             //loop through array from index 0, 
-            //and increase by 1 and run loop again until i is no longer less than size)
+            //and increase by 1 and loop again until i is no longer less than size - 1)
             for (int i = 0; i < size; i++){
                 //if array index = key, return the value of i
                 if (array [i] == key){
